cda6cb320188b53fd99c1f695ab656f946dbae47
Kevin Parmar <parmarkevinb@gmail.com>
2020-03-28 13:06:28 -0400

    Created route to homepage

--- server.js
+++ server.js
@@ -5,15 +5,14 @@ var app = express();
 
 var PORT = 8080;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-require("./controllers/html-routes");
-require("./controllers/api-routes");
+require("./controllers/html-routes")(app);
 
 app.listen(PORT, () => {
     console.log(PORT);
